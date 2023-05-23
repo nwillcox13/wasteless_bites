cb671d606174e8e66456427269262a15eddb5041
River <river.walsh33@gmail.com>
2023-05-23 13:49:05 -0500

    did item form and some css

--- ghi/src/ItemForm.js
+++ ghi/src/ItemForm.js
@@ -1,207 +1,189 @@
-import React, { useState, useEffect } from "react";
-function ItemForm() {
-    const [formData, setFormData] = useState({
-        name: '',
-        item_type: '',
-        quantity: '',
-        purchased_or_prepared: '',
-        time_of_post: '',
-        expiration: '',
-        location: '',
-        dietary_restriction: '',
-        description: '',
-        pickup_instructions: '',
-    });
-    const handleFormChange = (event) => {
-        const value = event.target.value;
-        const inputName = event.target.name;
-        setFormData({
-            ...formData,
-            [inputName]: value,
-        })
-    };
+import React, { useState } from "react";
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const newItemUrl = 'http://localhost:8000/items'
-        const itemData = {...formData}
-        const fetchConfig = {
-            method: "POST",
-            body: JSON.stringify(itemData),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }
-        console.log(itemData)
-        const response = await fetch (newItemUrl, fetchConfig);
-        if (response.ok) {
-            setFormData({
-                name: '',
-                item_type: '',
-                quantity: '',
-                purchased_or_prepared: '',
-                time_of_post: '',
-                expiration: '',
-                location: '',
-                dietary_restriction: '',
-                description: '',
-                pickup_instructions: '',
-        });
-    }
+function CondensedInput(props) {
+const { onChange, value, placeholder, type, name, id, label } = props;
+
+return (
+    <div className="col-md-6">
+    <label htmlFor={id} className="form-label">
+        {label}
+    </label>
+    <input
+        style={{ backgroundColor: "rgb(228, 230, 240)" }}
+        onChange={onChange}
+        value={value}
+        placeholder={placeholder}
+        required
+        type={type}
+        name={name}
+        id={id}
+        className="form-control"
+    />
+    </div>
+);
 }
 
-    return (
+function ItemForm() {
+const [formData, setFormData] = useState({
+    name: "",
+    item_type: "",
+    quantity: "",
+    purchased_or_prepared: "",
+    time_of_post: "",
+    expiration: "",
+    location: "",
+    dietary_restriction: "",
+    description: "",
+    pickup_instructions: "",
+});
+
+const handleFormChange = (event) => {
+    const { value, name } = event.target;
+    setFormData((prevData) => ({
+    ...prevData,
+    [name]: value,
+    }));
+};
+
+const handleSubmit = async (event) => {
+    event.preventDefault();
+    const newItemUrl = "http://localhost:8000/items";
+    const itemData = { ...formData };
+    const fetchConfig = {
+    method: "POST",
+    body: JSON.stringify(itemData),
+    headers: {
+        "Content-Type": "application/json",
+    },
+    };
+    console.log(itemData);
+    const response = await fetch(newItemUrl, fetchConfig);
+    if (response.ok) {
+    setFormData({
+        name: "",
+        item_type: "",
+        quantity: "",
+        purchased_or_prepared: "",
+        time_of_post: "",
+        expiration: "",
+        location: "",
+        dietary_restriction: "",
+        description: "",
+        pickup_instructions: "",
+    });
+    }
+};
+
+return (
     <div className="row">
-    <div className="offset-3 col-6">
-        <div className="shadow p-4 mt-4">
-        <h1>Create an item</h1>
-        <form onSubmit={handleSubmit} id="create-item-form">
-            <h1 className="mb-3">Add item</h1>
-            <div className="form-floating mb-3">
-                <label htmlFor="name">Name</label>
-                <input
-                    style={{ backgroundColor: "rgb(228, 230, 240)" }}
-                    onChange={handleFormChange}
-                    value={formData.name}
-                    placeholder="name"
-                    required
-                    type="text"
-                    name="name"
-                    id="name"
-                    className="form-control"
+    <div className="col-md-6 offset-md-3">
+        <div className="card shadow mt-4">
+        <div className="card-body">
+            <h1>Add an Item</h1>
+            <form onSubmit={handleSubmit} id="create-item-form">
+            <div className="row g-3">
+                <CondensedInput
+                onChange={handleFormChange}
+                value={formData.name}
+                placeholder="Name"
+                type="text"
+                name="name"
+                id="name"
+                label="Name"
+                />
+                <CondensedInput
+                onChange={handleFormChange}
+                value={formData.item_type}
+                placeholder="Item type"
+                type="text"
+                name="item_type"
+                id="item_type"
+                label="Item type"
+                />
+                <CondensedInput
+                onChange={handleFormChange}
+                value={formData.quantity}
+                placeholder="Quantity"
+                type="number"
+                name="quantity"
+                id="quantity"
+                label="Quantity"
+                />
+                <CondensedInput
+                onChange={handleFormChange}
+                value={formData.purchased_or_prepared}
+                placeholder="Purchased or Prepared"
+                type="datetime-local"
+                name="purchased_or_prepared"
+                id="purchased_or_prepared"
+                label="Purchased or Prepared"
+                />
+                <CondensedInput
+                onChange={handleFormChange}
+                value={formData.time_of_post}
+                placeholder="Time of post"
+                type="datetime-local"
+                name="time_of_post"
+                id="time_of_post"
+                label="Time of post"
+                />
+                <CondensedInput
+                onChange={handleFormChange}
+                value={formData.expiration}
+                placeholder="Expiration"
+                type="datetime-local"
+                name="expiration"
+                id="expiration"
+                label="Expiration"
+                />
+                <CondensedInput
+                onChange={handleFormChange}
+                value={formData.location}
+                placeholder="Location"
+                type="number"
+                name="location"
+                id="location"
+                label="Location"
+                />
+                <CondensedInput
+                onChange={handleFormChange}
+                value={formData.dietary_restriction}
+                placeholder="Dietary restrictions"
+                type="text"
+                name="dietary_restriction"
+                id="dietary_restriction"
+                label="Dietary restrictions"
+                />
+                <CondensedInput
+                onChange={handleFormChange}
+                value={formData.description}
+                placeholder="Item Description"
+                type="text"
+                name="description"
+                id="description"
+                label="Item Description"
+                />
+                <CondensedInput
+                onChange={handleFormChange}
+                value={formData.pickup_instructions}
+                placeholder="Pick-up Instructions"
+                type="text"
+                name="pickup_instructions"
+                id="pickup_instructions"
+                label="Pick-up Instructions"
                 />
             </div>
-            <div className="form-floating mb-3">
-                <label htmlFor="item_type">Item type</label>
-                <input
-                    style={{ backgroundColor: "rgb(228, 230, 240)" }}
-                    onChange={handleFormChange}
-                    value={formData.item_type}
-                    placeholder="item_type"
-                    required
-                    type="text"
-                    name="item_type"
-                    id="item_type"
-                    className="form-control"
-                />
+            <div className="col-12">
+                <button type="submit" className="btn btn-primary">
+                Add
+                </button>
             </div>
-            <div className="form-floating mb-3">
-                <label htmlFor="quantity">Quantity</label>
-                <input
-                    style={{ backgroundColor: "rgb(228, 230, 240)" }}
-                    onChange={handleFormChange}
-                    value={formData.quantity}
-                    placeholder="quantity"
-                    required
-                    type="number"
-                    name="quantity"
-                    id="quantity"
-                    className="form-control"
-                />
-            </div>
-            <div className="form-floating mb-3">
-                <label htmlFor="purchased_or_prepared">Purchased or Prepared</label>
-                <input
-                    style={{ backgroundColor: "rgb(228, 230, 240)" }}
-                    onChange={handleFormChange}
-                    value={formData.purchased_or_prepared}
-                    placeholder="purchased_or_prepared"
-                    required
-                    type="datetime-local"
-                    name="purchased_or_prepared"
-                    id="purchased_or_prepared"
-                    className="form-control"
-                />
-            </div>
-            <div className="form-floating mb-3">
-                <label htmlFor="time_of_post">Time of post</label>
-                <input
-                    style={{ backgroundColor: "rgb(228, 230, 240)" }}
-                    onChange={handleFormChange}
-                    value={formData.time_of_post}
-                    placeholder="time_of_post"
-                    required
-                    type="datetime-local"
-                    name="time_of_post"
-                    id="time_of_post"
-                    className="form-control"
-                />
-            </div>
-            <div className="form-floating mb-3">
-                <label htmlFor="expiration">Expiration</label>
-                <input
-                    style={{ backgroundColor: "rgb(228, 230, 240)" }}
-                    onChange={handleFormChange}
-                    value={formData.expiration}
-                    placeholder="expiration"
-                    required
-                    type="datetime-local"
-                    name="expiration"
-                    id="expiration"
-                    className="form-control"
-                />
-            </div>
-            <div className="form-floating mb-3">
-                <label htmlFor="location">Location</label>
-                <input
-                    style={{ backgroundColor: "rgb(228, 230, 240)" }}
-                    onChange={handleFormChange}
-                    value={formData.location}
-                    placeholder="location"
-                    required
-                    type="number"
-                    name="location"
-                    id="location"
-                    className="form-control"
-                />
-            </div>
-            <div className="form-floating mb-3">
-                <label htmlFor="dietary_restriction">Dietary restrictions</label>
-                <input
-                    style={{ backgroundColor: "rgb(228, 230, 240)" }}
-                    onChange={handleFormChange}
-                    value={formData.dietary_restriction}
-                    placeholder="dietary_restriction"
-                    required
-                    type="text"
-                    name="dietary_restriction"
-                    id="dietary_restriction"
-                    className="form-control"
-                />
-            </div>
-            <div className="form-floating mb-3">
-                <label htmlFor="description">Item Description</label>
-                <input
-                    style={{ backgroundColor: "rgb(228, 230, 240)" }}
-                    onChange={handleFormChange}
-                    value={formData.description}
-                    placeholder="description"
-                    required
-                    type="text"
-                    name="description"
-                    id="description"
-                    className="form-control"
-                />
-            </div>
-            <div className="form-floating mb-3">
-                <label htmlFor="pickup_instructions">Pick-up Instructions</label>
-                <input
-                    style={{ backgroundColor: "rgb(228, 230, 240)" }}
-                    onChange={handleFormChange}
-                    value={formData.pickup_instructions}
-                    placeholder="pickup_instructions"
-                    required
-                    type="text"
-                    name="pickup_instructions"
-                    id="pickup_instructions"
-                    className="form-control"
-                />
-            </div>
-            <button className="btn btn-secondary">Create</button>
             </form>
+        </div>
         </div>
     </div>
     </div>
-    );
+);
 }
+
 export default ItemForm;
--- journals/river.md
+++ journals/river.md
@@ -1,4 +1,4 @@
-##5-16-2023
+## 5-16-2023
 For the last week and a half, my group and I have been working effectively
 on our project. We started by discussing the application and then proceeded to
 wire-frame our desired layout. After completing the wire-framing, we individually
@@ -12,10 +12,23 @@ It has been a productive period, and we have made significant progress
 in fleshing out our idea, completing the necessary frameworks,
 and deciding on the appropriate technologies for our project.
 
-##5-17-2023
+## 5-17-2023
 Spent most of today working through the remainder of our .yaml file.
 Then after we were able to get that worked out
 we spent the rest of our day working on authentication with user login and logout.
 Also worked on the accounts setup.
 So far we have been fully working as a group, having one driver and
 the others observing and helping.
+
+## 5-22-2023
+Today I drove and made a form using react and JSX spent a lot of the day
+going over how we each did our last project in a different way
+I also started a List All Items page.
+
+
+## 5-23-2023
+Today we went over the things we had gone over yesterday,
+debugged a little bit, but one of us was having technical difficulties,
+so i decided to take the time to condense some of the code and apply some CSS and BootStrap
+may have decided upon a general theme for the website and possibly a Favicon
+
