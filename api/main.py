d5958a5963f40610ba3d66dd250a997f4e09aecf
Nick <nwillcox13@gmail.com>
2023-06-08 19:11:40 -0400

    storing minor changes and stability checking for the day

--- api/main.py
+++ api/main.py
@@ -4,7 +4,6 @@ from routers import accounts, items
 import chat
 from fastapi.middleware.cors import CORSMiddleware
 import os
-
 app = FastAPI()
 app.include_router(authenticator.router)
 app.include_router(accounts.router)
@@ -12,6 +11,7 @@ app.include_router(items.router)
 app.include_router(chat.router)
 origins = [
     "http://localhost:3000",
+    "https://nwillcox.gitlab.io",
     os.environ.get("CORS_HOST", None),
 ]
 app.add_middleware(
